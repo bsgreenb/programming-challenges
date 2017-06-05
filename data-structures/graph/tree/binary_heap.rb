e12b57d068d2186e63998aaf975596dbedef82ee
Ben Greenberg <bsgreenb@gmail.com>
2017-06-05 14:33:47 -0700

    Binary heap work

--- data-structures/graph/tree/binary_heap.rb
+++ data-structures/graph/tree/binary_heap.rb
@@ -1,72 +1,106 @@
 require_relative 'tree'
 
+# Array implementation has better time complexity but potentially worse space
+# complexity than the linked nodes implementation.  The array implementation
+# is also special and useful to know about.  So we'll implement Binary Heap
+# in that way here.
+
+# We will implement a max heap where values at top are always bigger than ones below.
+
+# Used https://www.sitepoint.com/heap-data-structure-ruby/ as a resource
+
 class Node
-  attr_accessor :value, :left_node, :right_node
+  attr_accessor :value
 
-  def initialize(val)
-    @value = val
-    @left_node = nil
-    @right_node = nil
+  def initialize(value)
+    @value = value
   end
-
-  def add_value(val)
-    if val <= @value
-      next_node = @left_node
-    else
-      next_node = @right_node
-    end
-
-    if next_node
-      next_node.add_value(val)
-    else
-      new_node = Node.new(val)
-      if val <= @value
-        @left_node = new_node
-      else
-        @right_node = new_node
-      end
-    end
-  end
-
 end
+
 
 class BinaryHeap
-  attr_accessor :root
+  attr_accessor :heap_arr
 
-  def initialize(val)
-    @root = Node.new(val)
+  def initialize
+    @heap_arr = []
   end
 
-  def add_value(val)
-    @root.add_value(val)
+
+  def left_child(index)
+    @heap_arr[2*index]
   end
 
-  # In Order Traversal (From least to most)
-  def in_order_traversal(node)
-    return [] unless node
-    in_order_traversal(node.left_node) + [ node.value ] + in_order_traversal(node.right_node)
+  def right_child(index)
+    @heap_arr[2*index + 1]
   end
 
-  # Pre-Order Traversal (Current node before children)
-  def pre_order_traversal(node)
-    return [] unless node
-    [ node.value ] + pre_order_traversal(node.left_node) + pre_order_traversal(node.right_node)
+  def parent(index)
+    return nil if index.zero?
+    @heap_arr[index/2]
   end
 
-  # Post-Order Traversal (Children before current node)
-  def post_order_traversal(node)
-    return [] unless node
-    pre_order_traversal(node.left_node) + pre_order_traversal(node.right_node) + [ node.value ]
+  def leaf_node?(index)
+    index >= @heap_arr.length / 2
   end
 
+  # Replace the [0]th element with the last one and bubble it down
+  def peek_max
+    @heap_arr[0]
+  end
+
+  # Push the element and bubble up.  This adds memory and then puts it in the right place.
+  def push(element)
+    offset = @heap_arr.length
+    node = Node.new(element)
+    @heap_arr << node
+
+    bubble_up(node)
+  end
+
+  # Recursively push an element up from the bottom of the tree as needed
+  def bubble_up(element)
+    #TODO: grabbing the parent.. I wanna make sure I do dat right..
+    offset = @heap_arr.length - 1
+
+    loop do
+      parent = offset / 2
+      break unless offset > 0 && @heap_arr[parent].value < element.value
+
+      # Swap
+      @heap_arr[parent], @heap_arr[offset] = @heap_arr[offset], @heap_arr[parent]
+    end
+  end
+
+  # Pop the element and bubble down.  We need to do this so that everything aintains its place in memory.
+  def pop_max
+    max_value = @heap_arr[0]
+
+    if @heap_arr.size > 0
+      @heap_arr[0] = @heap_arr.pop
+      bubble_down(@heap_arr[0])
+    end
+
+    return value
+  end
+
+  # Recursively push an element down the tree as needed
+  def bubble_down(node)
+    offset = 0
+    while (offset < @heap_arr.size / 2)
+      left_ch = left_child(offset)
+      right_ch = right_child(offset)
+
+      if (left_ch.value > right_ch.value)
+        larger_child = left_ch
+      else
+        larger_child = right_ch
+      end
+
+      if node.value >= larger_child.value
+        break
+      end
+
+      # Switch em
+      @heap_arr[offset], larger_child = larger_child, @heap_arr[offset]
+  end
 end
-
-tree = BinaryTree.new(13)
-tree.add_value(4)
-tree.add_value(50)
-tree.add_value(5)
-
-puts tree.inspect
-puts tree.in_order_traversal(tree.root).inspect
-puts tree.pre_order_traversal(tree.root).inspect
-puts tree.post_order_traversal(tree.root).inspect
